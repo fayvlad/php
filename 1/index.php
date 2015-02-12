4ba6ac3a51754775f546ba89584d26bf44655fb6
fayvlad <fayvlad@ukr.net>
2015-02-12 12:14:31 +0200

    add second task
    Second task- cut soname and father name for view Name S. F.

--- 1/index.php
+++ 1/index.php
@@ -27,6 +27,19 @@ if (isset($_POST['submit'])) {
         }
 
     }
+    if (strlen($_POST['fio']) > 0) {
+        $secondtask = 'block';
+        $fio = $_POST['fio'];
+        $fio = mb_convert_case($fio, MB_CASE_TITLE, "UTF-8");
+        $fio = explode(' ', $fio);
+        $fname = $fio[0];
+        $fsoname = $fio[1];
+        $fsoname = mb_substr($fsoname, 0, 2);
+        $fparent = $fio[2];
+        $fparent = mb_substr($fparent, 0, 2);
+        $fioresult = $fname.' '.$fsoname.'. '.$fparent.'.';
+    }
+}
 
 ?>
 
@@ -45,4 +58,11 @@ if (isset($_POST['submit'])) {
     <h3><p>Число = <?= $number ?></p></h3>
     <h4><p>Квадраты чисел</p><?= $result ?></h4>
     <h4><p>Сумма квадратов чисел</p><?= $summresult ?></h4>
+</div>
+
+
+<div class="secondtask" style="display: <?= $secondtask ?>">
+    <h3>Сокращенное ФИО</h3>
+
+    <p><?= $fioresult ?></p>
 </div>
